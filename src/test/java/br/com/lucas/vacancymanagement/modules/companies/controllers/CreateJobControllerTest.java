b31108828481a14f2a35bc0e30d90079aab345b9
Lucas Ferreira <lucasgomm33@gmail.com>
2024-06-27 01:05:30 -0300

    add test integration to create job

---
+++ src/test/java/br/com/lucas/vacancymanagement/modules/companies/controllers/CreateJobControllerTest.java
@@ -0,0 +1,69 @@
+package br.com.lucas.vacancymanagement.modules.companies.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import br.com.lucas.vacancymanagement.modules.companies.dtos.CreateJobDTO;
+import br.com.lucas.vacancymanagement.modules.companies.entities.CompanyEntity;
+import br.com.lucas.vacancymanagement.modules.companies.repositories.CompanyRepository;
+import br.com.lucas.vacancymanagement.modules.utils.TestUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class CreateJobControllerTest {
+
+  private MockMvc mvc;
+
+  @Autowired
+  private WebApplicationContext context;
+
+  @Autowired
+  private CompanyRepository companyRepository;
+
+  @Before
+  public void setup() {
+    mvc = MockMvcBuilders.webAppContextSetup(context).apply(SecurityMockMvcConfigurers.springSecurity()).build();
+  }
+
+  @Test
+  public void should_be_able_to_create_a_new_job() throws Exception {
+    var company = CompanyEntity.builder()
+                               .name("COMPANY_NAME")
+                               .username("COMPANY_USERNAME")
+                               .description("COMPANY_DESCRIPTION")
+                               .email("company@test.com")
+                               .password("12345")
+                               .build();
+
+    company = this.companyRepository.saveAndFlush(company);
+
+    var createJobDTO = CreateJobDTO.builder()
+                                    .benefits("BENEFITS_TEST")
+                                    .description("DESCRIPTION_TEST")
+                                    .level("LEVEL_TEST")
+                                    .build();
+
+    mvc.perform(MockMvcRequestBuilders.post("/companies/jobs/")
+                                      .contentType(MediaType.APPLICATION_JSON)
+                                      .content(TestUtils.objectToJSON(createJobDTO))
+                                      .header("Authorization", TestUtils.generateToken(company.getId(), "JAVAGAS_@123#")))
+                                      .andExpect(MockMvcResultMatchers.status().isOk());
+
+    
+  }
+}
+ 
